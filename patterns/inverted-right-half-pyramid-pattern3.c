38a962817fea157352f8b1829dfc77b162ffaafc
Mann Shah <mannshah4798@gmail.com>
2026-01-10 22:55:06 +0530

    Added new C programs and updated logic

---
+++ patterns/inverted-right-half-pyramid-pattern3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+
+    for(char i = 'A'; i <= 'E'; i++){
+
+        for(char j = i; j >= 'A'; j--){
+
+            printf("%c ",j);
+
+        }
+        printf("\n");
+
+
+    }
+
+
+
+
+    return 0;
+}
