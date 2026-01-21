fba64a6dbf9d22fca90f3974deec8d3062a59bb8
Mann Shah <mannshah4798@gmail.com>
2026-01-21 20:55:20 +0530

    updates

---
+++ patterns/with-space patterns/left-half-pyramid-pattern7.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int main () {
+
+        int n = 5;
+    for(int i = 1; i <= n; i++) {
+        
+        for (int j = 1; j <= n + 1 - i ; j++)
+        {
+            printf("%d",j);
+        }
+        for (int k = 1; k <= 2*(i-1); k++)
+        {
+            printf(" ");
+        }
+        for (int j = n - i + 1; j >= 1 ; j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+
+    
+return 0;
+}
